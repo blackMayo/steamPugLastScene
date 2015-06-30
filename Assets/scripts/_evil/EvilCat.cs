9964a5fa249c658d768c07c1029bf272a7e5c980
blackMayo <ioanna.vletsou@mail.com>
2015-06-30 22:26:54 +0200

    Cat is finally jumping to the right positions

--- Assets/scripts/_evil/EvilCat.cs
+++ Assets/scripts/_evil/EvilCat.cs
@@ -23,11 +23,9 @@ public class EvilCat : MonoBehaviour
 
 		AddVectorsToList ();
 		CalculateNewPosition ();
-		Debug.Log ("StartPosition = " + startPos + " Endposition = " + endPos);
 
 		// this is just used to instantiate the health of the evil cat as soon as the evil cat itself is instatiated
 		evilHealth = EvilHealth.Instance;
-
 	}
 	
 	protected void Update() {
@@ -61,7 +59,8 @@ public class EvilCat : MonoBehaviour
 		}
 		else {
 			endPos = newPositionFromList;
-		}	
+		}
+		Debug.Log ("StartPosition = " + startPos + " Endposition = " + endPos);
 	}
 	
 	void IncrementTimer ()
@@ -75,14 +74,29 @@ public class EvilCat : MonoBehaviour
 	
 	void AddVectorsToList ()
 	{
-		positionsList.Add (new Vector3(10.5f, 1.7f, 0.4f)); //Hay
-		positionsList.Add (new Vector3(11f, 5.2f, -0.9f)); // Table
-		positionsList.Add (new Vector3(11.76f, 6.27f, 0.95f)); // Barrel
-		positionsList.Add (new Vector3(12f, 9.4f, -0.315f)); // Ladder
-		positionsList.Add (new Vector3(12.3f, 12.3f, -1.3544f)); // FloorCage
-		positionsList.Add (new Vector3(12.4f, 13.45f, 1.904f)); // Pillar Back-Front
-		positionsList.Add (new Vector3(13f, 18.15f, 1.53f)); // Pillar Back-Back
-		positionsList.Add (new Vector3(12.4f, 25.2f, -2.5f)); // Pillar Front-Back
+		var ladder = GameObject.Find ("Quad_Ladder").transform.position;
+		positionsList.Add (new Vector3(ladder.x, ladder.y + 1.17f, ladder.z)); // Ladder
+
+		var hay = GameObject.Find ("Hay_B").transform.position;
+		positionsList.Add (new Vector3(hay.x, hay.y + 1.17f, hay.z)); //Hay
+
+		var table = GameObject.Find ("Plane_Table").transform.position;
+		positionsList.Add (new Vector3(table.x, table.y + 1.17f, table.z)); // Table
+
+		var barrel = GameObject.Find ("Plane_Barrel").transform.position;
+		positionsList.Add (new Vector3(barrel.x, barrel.y + 1.17f, barrel.z)); // Barrel
+
+		var floorCage = GameObject.Find ("Quad_FloorCage").transform.position;
+		positionsList.Add (new Vector3(floorCage.x, floorCage.y + 1.17f, floorCage.z)); // FloorCage
+
+		var pillarD1 = GameObject.Find ("Quad_PillarD1").transform.position;
+		positionsList.Add (new Vector3(pillarD1.x, pillarD1.y + 1.17f, pillarD1.z)); // Pillar Back-Front
+
+		var pillarD2 = GameObject.Find ("Quad_PillarD2").transform.position;
+		positionsList.Add (new Vector3(pillarD2.x, pillarD2.y + 1.17f, pillarD2.z)); // Pillar Back-Back
+
+		var pillarD = GameObject.Find ("Quad_PillarD").transform.position;
+		positionsList.Add (new Vector3(pillarD.x, pillarD.y + 1.17f, pillarD.z)); // Pillar Front-Back
 	}
 	
 	Vector3 CalculatePositionFromList () {
